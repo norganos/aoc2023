28f23598ded0d48fc2fbfd2cc48b8c8fb48bf8ee
Stephan Linkel <251381+norganos@users.noreply.github.com>
2023-12-05 06:35:55 +0100

    day 5 in a ridiculously inefficiently way

--- src/main/kotlin/de/linkel/aoc/Day05.kt
+++ src/main/kotlin/de/linkel/aoc/Day05.kt
@@ -5,10 +5,76 @@ import de.linkel.aoc.base.QuizPart
 import jakarta.inject.Singleton
 
 @Singleton
-class Day05: AbstractLinesAdventDay<Int>() {
+class Day05: AbstractLinesAdventDay<Long>() {
     override val day = 5
+    private val spaces = Regex("\\s+")
 
-    override fun process(part: QuizPart, lines: Sequence<String>): Int {
-        return 0
+    override fun process(part: QuizPart, lines: Sequence<String>): Long {
+        val iterator = lines.iterator()
+        val seedsLineValues = iterator.next().substringAfter(":").split(spaces).filter { it.isNotEmpty() }.map { it.toLong() }
+        val seeds = if (part == QuizPart.A) seedsLineValues.map { LongRange(it, it) }
+                else seedsLineValues.windowed(2, step = 2).map { LongRange(it[0], it[0] + it[1] - 1) }
+        iterator.next()
+        assert(iterator.next().trim() == "seed-to-soil map:")
+        val seedToSoil = consumeUntilDelimiter(iterator, "soil-to-fertilizer map:")
+        val soilToFertilizer = consumeUntilDelimiter(iterator, "fertilizer-to-water map:")
+        val fertilizerToWater = consumeUntilDelimiter(iterator, "water-to-light map:")
+        val waterToLight = consumeUntilDelimiter(iterator, "light-to-temperature map:")
+        val lightToTemperature = consumeUntilDelimiter(iterator, "temperature-to-humidity map:")
+        val temperatureToHumidity = consumeUntilDelimiter(iterator, "humidity-to-location map:")
+        val humidityToLocation = consumeUntilDelimiter(iterator, "EOF")
+
+        return seeds.minOf { seedRange ->
+            seedRange.minOf { seed ->
+                humidityToLocation.lookup(
+                        temperatureToHumidity.lookup(
+                            lightToTemperature.lookup(
+                                waterToLight.lookup(
+                                    fertilizerToWater.lookup(
+                                        soilToFertilizer.lookup(
+                                            seedToSoil.lookup(seed)
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                }
+            }
     }
+
+    private fun consumeUntilDelimiter(iterator: Iterator<String>, delimiter: String): Translation {
+        val result = mutableListOf<Mapping>()
+        while (iterator.hasNext()) {
+            val line = iterator.next().trim()
+            if (line.isEmpty()) continue
+            if (line == delimiter) break
+            val vals = line.split(spaces).map { it.trim().toLong() }
+            result.add(
+                Mapping(
+                    destRange = LongRange(vals[0], vals[0] + vals[2] - 1),
+                    sourceRange = LongRange(vals[1], vals[1] + vals[2] - 1)
+                )
+            )
+        }
+        return Translation(result)
+    }
+
+    data class Translation(
+        val mappings: List<Mapping>
+    ) {
+        fun lookup(input: Long): Long {
+            for (mapping in mappings) {
+                if (input in mapping.sourceRange) {
+                    return mapping.destRange.first + (input - mapping.sourceRange.first)
+                }
+            }
+            return input
+        }
+    }
+
+    data class Mapping(
+        val sourceRange: LongRange,
+        val destRange: LongRange
+    )
 }
--- src/test/kotlin/de/linkel/aoc/Day05Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day05Test.kt
@@ -1,13 +1,46 @@
 package de.linkel.aoc
 
-class Day05Test: AbstractDayTest<Int>() {
+class Day05Test: AbstractDayTest<Long>() {
     override val exampleA = """
-        """.trimIndent()
-    override val exampleSolutionA = 0
-    override val solutionA = 0
+seeds: 79 14 55 13
 
-    override val exampleSolutionB = 0
-    override val solutionB = 0
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+        """.trimIndent()
+    override val exampleSolutionA = 35L
+    override val solutionA = 107430936L
+
+    override val exampleSolutionB = 46L
+    override val solutionB = 0L
 
     override val implementation = Day05()
 }
