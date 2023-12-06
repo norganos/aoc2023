bfc7ddceb43b1fb903454397916aa194250658e9
Stephan Linkel <251381+norganos@users.noreply.github.com>
2023-12-06 06:26:48 +0100

    day 6...

--- src/main/kotlin/de/linkel/aoc/Day06.kt
+++ src/main/kotlin/de/linkel/aoc/Day06.kt
@@ -5,10 +5,32 @@ import de.linkel.aoc.base.QuizPart
 import jakarta.inject.Singleton
 
 @Singleton
-class Day06: AbstractLinesAdventDay<Int>() {
+class Day06: AbstractLinesAdventDay<Long>() {
     override val day = 6
 
-    override fun process(part: QuizPart, lines: Sequence<String>): Int {
-        return 0
+    override fun process(part: QuizPart, lines: Sequence<String>): Long {
+        val spaces = Regex("\\s+")
+        val (times, distances) = lines
+            .map { line ->
+                if (part == QuizPart.A) {
+                    line.split(spaces)
+                        .drop(1)
+                        .map { it.toLong() }
+                } else {
+                    listOf(line.substring(11).replace(" ", "").toLong())
+                }
+            }
+            .toList()
+        val races = times.zip(distances).toList()
+        return races.fold(1) { p, r -> p * getPossibleWins(r) }
+    }
+
+    private fun getPossibleWins(race: Pair<Long,Long>): Long {
+        return (1L until race.first).fold(0L) { s, ms ->
+            val speed = ms
+            val remaining = race.first - ms
+            val distance = speed * remaining
+            if (distance > race.second) s + 1 else s
+        }
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day06Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day06Test.kt
@@ -1,13 +1,15 @@
 package de.linkel.aoc
 
-class Day06Test: AbstractDayTest<Int>() {
+class Day06Test: AbstractDayTest<Long>() {
     override val exampleA = """
+Time:      7  15   30
+Distance:  9  40  200
         """.trimIndent()
-    override val exampleSolutionA = 0
-    override val solutionA = 0
+    override val exampleSolutionA = 288L
+    override val solutionA = 1108800L
 
-    override val exampleSolutionB = 0
-    override val solutionB = 0
+    override val exampleSolutionB = 71503L
+    override val solutionB = 36919753L
 
     override val implementation = Day06()
 }
