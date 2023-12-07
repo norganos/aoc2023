78e78c0f51c149e0c9436ef710aa9f7c259a2d61
Stephan Linkel <251381+norganos@users.noreply.github.com>
2023-12-07 07:55:37 +0100

    day 7

--- src/main/kotlin/de/linkel/aoc/Day07.kt
+++ src/main/kotlin/de/linkel/aoc/Day07.kt
@@ -5,10 +5,100 @@ import de.linkel.aoc.base.QuizPart
 import jakarta.inject.Singleton
 
 @Singleton
-class Day07: AbstractLinesAdventDay<Int>() {
+class Day07: AbstractLinesAdventDay<Long>() {
     override val day = 7
 
-    override fun process(part: QuizPart, lines: Sequence<String>): Int {
-        return 0
+    enum class Type {
+        FIVE,
+        FOUR,
+        FULLHOUSE,
+        THREE,
+        TWOPAIR,
+        PAIR,
+        HIGH;
+
+        operator fun plus(jokers: Int): Type = when (jokers) {
+            0 -> this
+            1 -> when(this) {
+                FOUR -> FIVE
+                THREE -> FOUR
+                TWOPAIR -> FULLHOUSE
+                PAIR -> THREE
+                HIGH -> PAIR
+                else -> throw Exception("combination of $this + $jokers joker should not happen")
+            }
+            2 -> when(this) {
+                FULLHOUSE -> FIVE
+                TWOPAIR -> FOUR
+                PAIR -> THREE
+                else -> throw Exception("combination of $this + $jokers jokers should not happen")
+            }
+            3 -> when(this) {
+                FULLHOUSE -> FIVE
+                THREE -> FOUR
+                else -> throw Exception("combination of $this + $jokers jokers should not happen")
+            }
+            4 -> when(this) {
+                FOUR -> FIVE
+                else -> throw Exception("combination of $this + $jokers jokers should not happen")
+            }
+            5 -> when(this) {
+                FIVE -> FIVE
+                else -> throw Exception("combination of $this + $jokers jokers should not happen")
+            }
+            else -> throw Exception("combination of $this + $jokers jokers should not happen")
+        }
+    }
+
+    data class Hand(
+        val type: Type,
+        val hand: String,
+        val values: List<Int>,
+        val bid: Long
+    )
+    override fun process(part: QuizPart, lines: Sequence<String>): Long {
+        return lines
+            .map { line ->
+                val (hand, bid) = line.split(" ")
+                val amounts = hand.toCharArray()
+                    .groupBy { it }
+                    .entries
+                    .map { Pair(it.key, it.value.size) }
+                    .groupBy { it.second }
+                val jokers = if (part == QuizPart.A) 0 else hand.count { it == 'J' }
+                val type = (
+                        if (amounts.containsKey(5)) Type.FIVE
+                        else if (amounts.containsKey(4)) Type.FOUR
+                        else if (amounts.containsKey(3) && amounts.containsKey(2)) Type.FULLHOUSE
+                        else if (amounts.containsKey(3)) Type.THREE
+                        else if (amounts.containsKey(2) && amounts[2]!!.size > 1) Type.TWOPAIR
+                        else if (amounts.containsKey(2)) Type.PAIR
+                        else Type.HIGH
+                    ) + jokers
+
+                val values = hand.toCharArray()
+                    .map { c ->
+                        if (c.isDigit()) c.digitToInt()
+                        else when (c) {
+                            'T' -> 10
+                            'J' -> if (part == QuizPart.A) 11 else 1
+                            'Q' -> 12
+                            'K' -> 13
+                            'A' -> 14
+                            else -> throw Exception("invalid card $c")
+                        }
+                    }
+                    Hand(type, hand, values, bid.toLong())
+            }
+            .sortedWith { a, b ->
+                b.type.compareTo(a.type)
+                    .takeIf { it != 0 }
+                    ?: (0..4).firstNotNullOfOrNull { i ->
+                        a.values[i].compareTo(b.values[i])
+                            .takeIf { it != 0 }
+                    } ?: 0
+            }
+            .mapIndexed { idx, hand ->  hand.bid * (idx + 1) }
+            .sum()
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day07Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day07Test.kt
@@ -1,13 +1,18 @@
 package de.linkel.aoc
 
-class Day07Test: AbstractDayTest<Int>() {
+class Day07Test: AbstractDayTest<Long>() {
     override val exampleA = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
         """.trimIndent()
-    override val exampleSolutionA = 0
-    override val solutionA = 0
+    override val exampleSolutionA = 6440L
+    override val solutionA = 251287184L
 
-    override val exampleSolutionB = 0
-    override val solutionB = 0
+    override val exampleSolutionB = 5905L
+    override val solutionB = 250757288L
 
     override val implementation = Day07()
 }
