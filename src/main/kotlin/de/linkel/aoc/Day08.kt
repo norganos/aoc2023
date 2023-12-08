60ee8cb90beda74994ba645ce466e2cd76c2d856
Stephan Linkel <251381+norganos@users.noreply.github.com>
2023-12-08 14:18:30 +0100

    day 8 quick and dirty

--- src/main/kotlin/de/linkel/aoc/Day08.kt
+++ src/main/kotlin/de/linkel/aoc/Day08.kt
@@ -1,14 +1,63 @@
 package de.linkel.aoc
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators.IntSequenceGenerator
 import de.linkel.aoc.base.AbstractLinesAdventDay
 import de.linkel.aoc.base.QuizPart
 import jakarta.inject.Singleton
+import kotlin.math.max
+import kotlin.math.min
 
 @Singleton
-class Day08: AbstractLinesAdventDay<Int>() {
-    override val day = 7
+class Day08: AbstractLinesAdventDay<Long>() {
+    override val day = 8
 
-    override fun process(part: QuizPart, lines: Sequence<String>): Int {
-        return 0
+    data class NodeDesc(val left: String, val right: String)
+
+    override fun process(part: QuizPart, lines: Sequence<String>): Long {
+        val pattern = Regex("([A-Z0-9]+) = \\(([A-Z0-9]+), ([A-Z0-9]+)\\)")
+        val iterator = lines.iterator()
+        val instr = iterator.next()
+        assert(iterator.next() == "")
+        val nodes = iterator.asSequence()
+            .mapNotNull { pattern.matchEntire(it) }
+            .map { it.groupValues[1] to NodeDesc(it.groupValues[2], it.groupValues[3]) }
+            .toMap()
+
+        return if (part == QuizPart.A) {
+            countSteps(nodes, "AAA", setOf("ZZZ"), instr)
+        } else {
+            val end = nodes.keys
+                .filter { it.endsWith("Z") }
+                .toSet()
+            nodes.keys
+                .filter { it.endsWith("A") }
+                .also { println("start: ${it.joinToString(", ")}") }
+                .map { countSteps(nodes, it, end, instr) }
+                .also { println("steps: ${it.joinToString(", ")}") }
+                .reduce(::lcm)
+        }
+    }
+
+    private fun countSteps(nodes: Map<String, NodeDesc>, start: String, end: Set<String>, instr: String): Long {
+        var steps = 0L
+        var pos = start
+        while (pos !in end) {
+            val now = nodes[pos]!!
+            pos = if (instr[(steps % instr.length).toInt()] == 'L') now.left else now.right
+            steps++
+        }
+        return steps
+    }
+
+    private fun lcm(a: Long, b: Long): Long {
+        var lcm = max(a, b)
+
+        while (true) {
+            if (lcm % a == 0L && lcm % b == 0L) {
+                break
+            }
+            lcm += max(a, b)
+        }
+        return lcm
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day08Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day08Test.kt
@@ -1,13 +1,42 @@
 package de.linkel.aoc
 
-class Day08Test: AbstractDayTest<Int>() {
+class Day08Test: AbstractDayTest<Long>() {
     override val exampleA = """
-        """.trimIndent()
-    override val exampleSolutionA = 0
-    override val solutionA = 0
+RL
 
-    override val exampleSolutionB = 0
-    override val solutionB = 0
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+        """.trimIndent()
+    val exampleA2 = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+        """.trimIndent()
+    override val exampleSolutionA = 2L
+    override val solutionA = 14429L
+
+    override val exampleB = """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+    """.trimIndent()
+
+    override val exampleSolutionB = 6L
+    override val solutionB = 10921547990923L
 
     override val implementation = Day08()
 }
