bd3312cb8485ee70d969efce3d3616f07c0c37df
Stephan Linkel <251381+norganos@users.noreply.github.com>
2023-12-12 05:58:47 +0100

    prepared the next 2 days

---
+++ src/main/kotlin/de/linkel/aoc/Day14.kt
@@ -0,0 +1,14 @@
+package de.linkel.aoc
+
+import de.linkel.aoc.base.AbstractLinesAdventDay
+import de.linkel.aoc.base.QuizPart
+import jakarta.inject.Singleton
+
+@Singleton
+class Day14: AbstractLinesAdventDay<Int>() {
+    override val day = 14
+
+    override fun process(part: QuizPart, lines: Sequence<String>): Int {
+       return 0
+    }
+}
---
+++ src/test/kotlin/de/linkel/aoc/Day14Test.kt
@@ -0,0 +1,13 @@
+package de.linkel.aoc
+
+class Day14Test: AbstractDayTest<Int>() {
+    override val exampleA = """
+        """.trimIndent()
+    override val exampleSolutionA = 0
+    override val solutionA = 0
+
+    override val exampleSolutionB = 0
+    override val solutionB = 0
+
+    override val implementation = Day14()
+}
