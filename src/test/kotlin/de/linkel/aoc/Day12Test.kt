a2129ea4ee33f9cdcf4dde0ee214f15ff9aa492c
Stephan Linkel <251381+norganos@users.noreply.github.com>
2023-12-12 13:31:17 +0100

    did take way too long for such a ugly mess :-(

--- src/test/kotlin/de/linkel/aoc/Day12Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day12Test.kt
@@ -1,13 +1,31 @@
 package de.linkel.aoc
 
-class Day12Test: AbstractDayTest<Int>() {
-    override val exampleA = """
-        """.trimIndent()
-    override val exampleSolutionA = 0
-    override val solutionA = 0
+import java.math.BigInteger
 
-    override val exampleSolutionB = 0
-    override val solutionB = 0
+class Day12Test: AbstractDayTest<BigInteger>() {
+    override val exampleA = """
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+        """.trimIndent()
+    override val exampleSolutionA = 21L.toBigInteger()
+    override val solutionA = 7344L.toBigInteger()
+
+    override val exampleSolutionB = 525152L.toBigInteger()
+    override val solutionB = 1379793119L.toBigInteger()
 
     override val implementation = Day12()
 }
+/* wrong:
+6193
+7358
+7344
+22854629599L
+22854629599L
+22854629599
+1379793119L
+1088006519007
+ */
